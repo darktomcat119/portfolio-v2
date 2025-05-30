2c330aab407de27acc52f23deaddda251ca0f874
darktomcat119 <lukas020119@gmail.com>
2025-05-29 19:09:10 -0700

    fix: update send mail pard

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -16,7 +16,7 @@ export const QUIET_NODE_PORTFOLIO_V2_GITHUB_LINK =
 export const QUIET_NODE_GITHUB_LINK = 'https://github.com/darktomcat119';
 export const QUIET_NODE_REDDIT_LINK =
   'https://www.reddit.com/user/Quiet-Computer-3495';
-export const QUIET_NODE_EMAIL_LINK = 'mailto:denyskanivets79@gmail.com';
+export const QUIET_NODE_EMAIL_LINK = 'mailto:johndoe@fakeemail.com';
 export const QUIET_NODE_LINKEDIN_LINK = 'https://www.linkedin.com/in/denys-kanivets-profile/';
 export const QUIET_NODE_TWITTER_LINK = 'https://twitter.com/quiet_node';
 export const QUIET_NODE_DISCORD_LINK =
